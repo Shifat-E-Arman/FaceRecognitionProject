f108240b706146add830ca55ca3c6d77dd266f77
atahmeed <tambahamba@gmail.com>
2019-06-11 15:13:07 +0600

    knn report

--- learing.py
+++ learing.py
@@ -1,9 +1,9 @@
-import numpy as np
 import pandas as pd
-from sklearn.linear_model import LogisticRegression
+#from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split,GridSearchCV
 from sklearn.metrics import confusion_matrix,classification_report
-from sklearn.neighbors import KNeighborsClassifier
+#from sklearn.neighbors import KNeighborsClassifier
+from sklearn.svm import SVC
 
 
 df = pd.read_csv('final_bright.csv')
@@ -14,18 +14,42 @@ X = df.drop(['label'],axis=1)
 
 X_train,X_test,y_train,y_test = train_test_split(X,Y,test_size=0.3, random_state=42)
 
-param_grid = {'n_neighbors':np.arange(1,20)}
-knn=KNeighborsClassifier()
+tuned_parameters = [{'kernel': ['rbf'], 'gamma': [1e-3, 1e-4],
+                     'C': [1, 10, 100, 1000]},
+                    {'kernel': ['linear'], 'C': [1, 10, 100, 1000]}]
 
-gcv = GridSearchCV(knn, param_grid,cv=5)
+scores = ['roc_auc', 'recall','precision']
 
-gcv.fit(X_train,y_train)
+for score in scores:
+    print("# Tuning hyper-parameters for %s" % score)
+    print()
 
-print("Best n= ",gcv.best_params_)
-print("Best Score= ",gcv.best_score_)
+    clf = GridSearchCV(SVC(), tuned_parameters, cv=4,verbose=True,
+                       scoring=score)
+    clf.fit(X_train, y_train)
 
-y_pred = gcv.predict(X_test)
+    print("Best parameters set found on development set:")
+    print()
+    print(clf.best_params_)
+    print()
+    print("Grid scores on development set:")
+    print()
+    means = clf.cv_results_['mean_test_score']
+    stds = clf.cv_results_['std_test_score']
+    for mean, std, params in zip(means, stds, clf.cv_results_['params']):
+        print("%0.3f (+/-%0.03f) for %r"
+              % (mean, std * 2, params))
+    print()
+    print("Confusion Matrix")
+    print("----------------")
+    print(confusion_matrix(y_test,y_pred))
+    print()
+    print("Detailed classification report:")
+    print()
+    print("The model is trained on the full development set.")
+    print("The scores are computed on the full evaluation set.")
+    print()
+    y_true, y_pred = y_test, clf.predict(X_test)
+    print(classification_report(y_true, y_pred))
+    print()
 
-print("Confusion Matrix")
-print("----------------")
-print(confusion_matrix(y_test,y_pred))
