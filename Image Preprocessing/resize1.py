5eb573a71e2c189e3f97baabd9e479512e6b90d6
Shifat E Arman <shifatearman@gmail.com>
2019-06-12 16:20:50 +0600

    resized images and ground truths are added

--- Image Preprocessing/resize1.py
+++ Image Preprocessing/resize1.py
@@ -1,24 +1,28 @@
 import cv2
 import os
 
-images = os.listdir('image_database/')
-
-os.mkdir('image_database/Resized')    
+images = os.listdir('image_database/resized')
+for files in images:
+    if ('jpg' not in files) or ('JPG' not in files):
+        images.remove(files)
+        
+os.mkdir('image_database/resized/again')    
 couldnot=[]
 count = 0
-for x in range(84):
-    img = cv2.imread('image_database/'+images[x],cv2.IMREAD_UNCHANGED)
+for x in range(len(images)):
+    img = cv2.imread('image_database/resized/'+images[x],cv2.IMREAD_UNCHANGED)
+    
     try:
-        newimg = cv2.resize(img, (226,226) , interpolation=cv2.INTER_AREA)
+        newimg = cv2.resize(img, (28,28) , interpolation=cv2.INTER_AREA)
         count = count+1
-        cv2.imwrite('image_database/Resized/'+images[x],newimg)    
+        cv2.imwrite('image_database/resized/again/'+images[x],newimg)    
     
     except:
         print("Could not Resize ", images[x])
         couldnot.append(images[x])
         
     print(images[x])
-    
+        
     
 print(count)
 #newfile = open('error.txt','w')
